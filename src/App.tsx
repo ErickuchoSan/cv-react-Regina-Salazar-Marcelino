2bf2ee302cbbc6a635b0ae52aea0ac47f449bf07
Erick Bores <e.bores.i@outlook.com>
2026-07-20 00:44:33 -0600

    feat: wire VersionSwitcher and conditional rendering into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-
+import { useVersion } from './hooks/useVersion';
 import { RHVersion } from './components/RHVersion';
+import { InfantilVersion } from './components/Infantil/InfantilVersion';
+import { VersionSwitcher } from './components/UI/VersionSwitcher';
 
 function App() {
-  return <RHVersion />;
+    const { version, toggleVersion } = useVersion();
+
+    return (
+        <>
+            {version === 'rh' ? <RHVersion /> : <InfantilVersion />}
+            <VersionSwitcher version={version} onToggle={toggleVersion} />
+        </>
+    );
 }
 
 export default App;
