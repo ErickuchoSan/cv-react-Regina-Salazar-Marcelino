552c55b54f1540fbc6815d7ad3cc2cec69148dc8
Erick Bores <e.bores.i@outlook.com>
2026-07-19 23:05:22 -0600

    refactor: extract RHVersion component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
 
-import { Navbar } from './components/Layout/Navbar';
-import { Hero } from './components/Sections/Hero';
-import { About } from './components/Sections/About';
-import { Experience } from './components/Sections/Experience';
-import { Skills } from './components/Sections/Skills';
-import { Languages } from './components/Sections/Languages';
-import { Projects } from './components/Sections/Projects';
-import { Contact } from './components/Sections/Contact';
-import { Footer } from './components/Sections/Footer';
+import { RHVersion } from './components/RHVersion';
 
 function App() {
-
-
-  return (
-    <div className="min-h-screen">
-      <Navbar />
-      <main>
-        <Hero />
-        <Experience />
-        <About />
-        <Projects />
-        <Skills />
-        <Languages />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <RHVersion />;
 }
 
 export default App;
